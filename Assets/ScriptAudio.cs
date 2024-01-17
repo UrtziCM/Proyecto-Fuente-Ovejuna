7fd2594dbebc77551484cf8882315b6b4972f99e
Alejandro Millan <alejandromillanmolinero@gmail.com>
2024-01-17 13:34:25 +0100

    UPDATE lo he arreglado, ya va👍😃 ( a medias, realmente queda arreglar que dependiendo de la escena de la que venga se ponga un audio u otro, ya que realmente de momento siempre saca el mismo audio🥸)

--- Assets/ScriptAudio.cs
+++ Assets/ScriptAudio.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 public class ScriptAudio : MonoBehaviour
@@ -15,36 +14,19 @@ public class ScriptAudio : MonoBehaviour
     private bool isPlaying = false;
     private bool isDragging = false;
 
-    // Diccionario que mapea los nombres de escena a índices de audio
-    private Dictionary<string, int> sceneAudioIndexMapping;
-
     void Start()
     {
-        // Inicializar el diccionario de mapeo
-        InitializeSceneAudioMapping();
-
         botonJarraitu = gameObject.transform.Find("BotonJarraitu").gameObject.GetComponent<Button>();
         botonJarraitu.onClick.AddListener(JuegoTerminado);
         audioSource = GetComponent<AudioSource>();
         playPauseButton.onClick.AddListener(TogglePlayPause);
         audioSlider.onValueChanged.AddListener(OnSliderValueChanged);
 
-        // Recupera el nombre de la escena actual
-        string currentSceneName = SceneManager.GetActiveScene().name;
-
-        // Verifica si el nombre de la escena existe en el diccionario
-        if (sceneAudioIndexMapping.ContainsKey(currentSceneName))
+        // Carga el audio correspondiente a la escena actual
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex < audioClips.Length)
         {
-            int audioIndex = sceneAudioIndexMapping[currentSceneName];
-
-            if (audioIndex < audioClips.Length)
-            {
-                audioSource.clip = audioClips[audioIndex];
-            }
-        }
-        else
-        {
-            Debug.LogError("No se encontró un índice de audio para la escena: " + currentSceneName);
+            audioSource.clip = audioClips[sceneIndex];
         }
     }
 
@@ -88,21 +70,6 @@ public class ScriptAudio : MonoBehaviour
 
     public void JuegoTerminado()
     {
-        // Almacena el índice de la escena actual como la escena anterior
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        PlayerPrefs.SetInt("PreviousSceneIndex", currentSceneIndex);
-    }
-
-    // Método para inicializar el diccionario de mapeo
-    void InitializeSceneAudioMapping()
-    {
-        sceneAudioIndexMapping = new Dictionary<string, int>();
-
-        // Asocia nombres de escena con índices de audio
-        sceneAudioIndexMapping.Add("PreguntasTexto", 4);
-        sceneAudioIndexMapping.Add("PuzzleOntziolak", 1);
-        sceneAudioIndexMapping.Add("PuzzleAbuelas", 3);
-        sceneAudioIndexMapping.Add("EscenaJuntarBotones", 2);
-        // Agrega más según sea necesario
+        
     }
 }
